6b4c8aaa5543236c9cbd33d056f17902dea663cf
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2025-01-04 22:48:37 +0530

    Added all UnrealMemory functions to the UnrealEssentials Interface
    As far as I know, the game doesn't crash while I'm trying to use these. Can't confirm if they all work as they should so testing is needed.

--- UnrealEssentials.Interfaces/IUnrealEssentials.cs
+++ UnrealEssentials.Interfaces/IUnrealEssentials.cs
@@ -1,10 +1,55 @@
 ﻿namespace UnrealEssentials.Interfaces;
-public interface IUnrealEssentials
+public unsafe interface IUnrealEssentials
 {
     /// <summary>
     /// Adds files from the folder at <paramref name="path"/>. 
     /// This folder is treated like it was the UnrealEssentials folder inside of a mod.
     /// </summary>
     /// <param name="path">Path to the folder that contains files to be loaded.</param>
-    public void AddFromFolder(string path);
+    void AddFromFolder(string path);
+
+    //!!! WARNING FOR ALL MEMORY RELATED FUNCTIONS !!!
+    //THEY SHOULD ONLY BE CALLED AFTER THE GAME HAS LAUNCHED. TRYING TO USE THEM BEFORE THE GAME HAS LAUNCHED WILL RESULT IN A CRASH.
+
+    /// <summary>
+    /// Allocates a piece of memory. <paramref name="Count" name="Alignment"/>. 
+    /// </summary>
+    /// <param name="Count">Number of bytes to allocate.</param>
+    /// <param name="Alignment">Alignment of the allocation.</param>
+    void* Malloc(nuint Count, uint Alignment = 0);
+
+    /// <summary>
+    /// Allocates a piece of memory. <paramref name="Count" name="Alignment"/>. 
+    /// </summary>
+    /// <param name="Count">Number of bytes to allocate.</param>
+    /// <param name="Alignment">Alignment of the allocation.</param>
+    void* TryMalloc(nuint Count, uint Alignment = 0);
+
+    /// <summary>
+    /// Reallocates a piece of memory. <paramref name="Count" name="Alignment" name="Original"/>. 
+    /// </summary>
+    /// <param name="Original">Pointer to the original memory.</param>
+    /// <param name="Count">Number of bytes to allocate.</param>
+    /// <param name="Alignment">Alignment of the allocation.</param>
+    void* Realloc(void* Original, nuint Count, uint Alignment = 0);
+
+    /// <summary>
+    /// Reallocates a piece of memory. <paramref name="Count" name="Alignment" name="Original"/>. 
+    /// </summary>
+    /// <param name="Original">Pointer to the original memory.</param>
+    /// <param name="Count">Number of bytes to allocate.</param>
+    /// <param name="Alignment">Alignment of the allocation.</param>
+    void* TryRealloc(void* Original, nuint Count, uint Alignment = 0);
+
+    /// <summary>
+    /// Frees a piece of memory. <paramref name="Original"/>. 
+    /// </summary>
+    /// <param name="Original">Pointer to the original memory.</param>
+    void Free(void* Original);
+
+    nuint QuantizeSize(nuint Count, uint Alignment);
+
+    bool GetAllocationSize(void* Original, nuint* SizeOut);
+
+    void Trim(bool bTrimThreadCaches);
 }
--- UnrealEssentials/Api.cs
+++ UnrealEssentials/Api.cs
@@ -1,8 +1,25 @@
 ﻿using UnrealEssentials.Interfaces;
+using UnrealEssentials.Unreal;
 
 namespace UnrealEssentials;
-public class Api : IUnrealEssentials
+public unsafe class Api : IUnrealEssentials
 {
+    public void* Malloc(nuint Count, uint Alignment = 0) => UnrealMemory.Malloc(Count, Alignment);
+
+    public void* TryMalloc(nuint Count, uint Alignment = 0) => UnrealMemory.TryMalloc(Count, Alignment);
+
+    public void* Realloc(void* Original, nuint Count, uint Alignment = 0) => UnrealMemory.Realloc(Original, Count, Alignment);
+
+    public void* TryRealloc(void* Original, nuint Count, uint Alignment = 0) => UnrealMemory.TryRealloc(Original, Count, Alignment);
+
+    public void Free(void* Original) => UnrealMemory.Free(Original);
+
+    public nuint QuantizeSize(nuint Count, uint Alignment) => UnrealMemory.QuantizeSize(Count, Alignment);
+
+    public bool GetAllocationSize(void* Original, nuint* SizeOut) => UnrealMemory.GetAllocationSize(Original, SizeOut);
+
+    public void Trim(bool bTrimThreadCaches) => UnrealMemory.Trim(bTrimThreadCaches);
+
     private Action<string> _addFolder;
 
     internal Api(Action<string> addFolder)
